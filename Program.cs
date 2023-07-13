2f07cfd529a65a35732b741c0b081eb144f7ebb4
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-07-14 00:09:11 +0200

    Add game and readme

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramGameApp
+{
+    // Interface for the anagram game
+    public interface IAnagramGame
+    {
+        void StartGame();
+    }
+
+    // Class responsible for handling the anagram game logic
+    public class AnagramGame : IAnagramGame
+    {
+        private readonly List<string> words;
+        private static readonly Random random = new();
+
+        private int score;
+
+        public AnagramGame(List<string> words)
+        {
+            this.words = words;
+            score = 0;
+        }
+
+        public void StartGame()
+        {
+            Console.WriteLine("Welcome to the White Stripes Anagram Game!");
+            Console.WriteLine($"Score: {score}");
+
+            bool playAgain = true;
+
+            while (playAgain && words.Count > 0)
+            {
+                string selectedWord = GetRandomWord();
+                string anagram = GenerateAnagram(selectedWord);
+
+                Console.WriteLine("Guess the word:");
+                Console.WriteLine($"Anagram: {anagram}");
+                string guess = ReadUserInputToLower();
+                int attempts = 1;
+                bool hintGiven = false;
+
+                while (guess != selectedWord && attempts < 3)
+                {
+                    Console.WriteLine("Oops! That's incorrect.");
+
+                    if (!hintGiven)
+                    {
+                        Console.WriteLine("Do you want a hint? (yes/no)");
+                        string hintInput = ReadUserInputToLower();
+
+                        if (hintInput == "yes")
+                        {
+                            Console.WriteLine($"Hint: The first letter is {selectedWord[0]}");
+                            hintGiven = true;
+                        }
+                    }
+
+                    Console.WriteLine("Guess the word again:");
+                    guess = ReadUserInputToLower();
+                    attempts++;
+                }
+
+                if (guess == selectedWord)
+                {
+                    Console.WriteLine("Congratulations! You guessed it right!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you didn't guess correctly within the given attempts.");
+                    Console.WriteLine($"The word was: {selectedWord}");
+                }
+
+                Console.WriteLine($"Score: {score}");
+
+                if (words.Count > 0)
+                {
+                    Console.WriteLine("Play another word? (yes/no)");
+                    string playAgainInput = ReadUserInputToLower();
+                    // Determine if the user wants to play another word based on their input
+                    playAgain = playAgainInput == "yes";
+                }
+                else
+                {
+                    Console.WriteLine("No more words to play.");
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine($"Your final score: {score}");
+            Console.WriteLine("Thank you for playing!");
+        }
+
+        private string GetRandomWord()
+        {
+            int index = random.Next(0, words.Count);
+            string word = words[index];
+            words.RemoveAt(index);
+            return word;
+        }
+
+        private static string GenerateAnagram(string word)
+        {
+            char[] chars = word.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int j = random.Next(i, chars.Length);
+                (chars[j], chars[i]) = (chars[i], chars[j]);
+            }
+
+            return new string(chars);
+        }
+
+        private static string ReadUserInputToLower()
+        {
+            return (Console.ReadLine() ?? string.Empty).ToLower();
+        }
+    }
+
+    // Entry point of the program
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> words = new() { "acorn", "aluminum", "apple", "biscuit", "blossom", "button", "denial", "dirty", "elephant", "faith", "finger", "forever", "friend", "ghost", "gentleman", "ground", "hello", "hotel", "infirmary", "little", "lonely", "martyr", "medicine", "nation", "offend", "operator", "orchid", "pocket", "pretty", "protector", "sister", "special", "stripe", "thump", "truth", "twist", "union", "white" };
+            IAnagramGame anagramGame = new AnagramGame(words);
+            anagramGame.StartGame();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# White Stripes Anagram Game
+
+A console app game built in C#.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Customization](#customization)
+- [Tools](#tools)
+- [Credits](#credits)
+
+## Installation
+
+1. Clone the repository onto your machine:
+   `git clone https://github.com/your-username/project.git`
+   or copy the contents of the `Program.cs` file into a new `.cs` file.
+2. Navigate to the correct folder: `cd your-path-here/SongsAnagramGame`
+3. Run the project: `dotnet run`
+
+## Usage
+
+The player can use the terminal to play the game. The goal is to guess the anagrammed word, which is randomly chosen from a list of words taken from White Stripes song titles, in three attempts or less.
+
+<!-- to be completed -->
+
+## Features
+
+<!-- to be added -->
+
+## Customization
+
+<!-- to be added -->
+
+## Tools
+
+To write the app, I used Microsoft's [Visual Studio](https://visualstudio.microsoft.com/) IDE.
+
+You can chose another C# compatible IDE - for example, [Visual Studio Code](https://code.visualstudio.com/).
+
+## Credits
+
+<!-- White Stripes credits -->
